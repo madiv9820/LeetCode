a0962117996023a395a2c2fb07ee7b1e7ed0df7e
madiv9820 <themathur901@gmail.com>
2024-01-25 18:24:33 +0530

    Optimized Solution Using Inbuilt Cache

--- Daily/1143. Longest Common Subsequence/Solution1.py
+++ Daily/1143. Longest Common Subsequence/Solution1.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         '''
@@ -5,9 +6,11 @@ class Solution:
             Time Complexity: O(M*N)
             Space Complexity: O(M*N)
         '''
+        '''
         text1_Size, text2_Size = len(text1) + 1, len(text2) + 1
         cache = [[-1] * text2_Size for _ in range(text1_Size)]
         
+        @lru_cache(maxsize = None)
         def LCS(text1: str, text2: str) -> int:
             if len(text1) and len(text2):
                 if cache[len(text1)][len(text2)] == -1:
@@ -18,6 +21,16 @@ class Solution:
             return 0
         
         return LCS(text1, text2)
+        '''
+        @lru_cache(maxsize=None)
+        def LCS(p1, p2):
+            if p1 == len(text1) or p2 == len(text2):
+                return 0
+            if text1[p1] == text2[p2]:
+                return 1 + LCS(p1 + 1, p2 + 1)
+            else:
+                return max(LCS(p1, p2 + 1), LCS(p1 + 1, p2))
+        return LCS(0, 0)
 
 if __name__ == '__main__':
     text1, text2 = tuple(input().split()); sol = Solution()
